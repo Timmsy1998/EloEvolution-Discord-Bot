6ae636b81e1c38d3d3a793c1b4d9cffbf58cf8c1
James Timms <59345798+Timmsy1998@users.noreply.github.com>
2023-11-01 21:53:17 +0000

    Created Restart Command. Refactored index.js to look nicer

---
+++ commands/restart.js
@@ -0,0 +1,18 @@
+const { CommandInteraction } = require('discord.js');
+
+module.exports = {
+  data: {
+    name: 'restart',
+    description: 'Restart the bot.',
+  },
+
+  async execute(interaction) {
+    const allowedUserId = '789389975308730398';
+    if (interaction.user.id === allowedUserId) {
+      await interaction.reply('Restarting the bot...');
+      process.exit(0);
+    } else {
+      await interaction.reply('You are not authorized to use this command.');
+    }
+  },
+};
--- index.js
+++ index.js
@@ -1,49 +1,54 @@
-const fs = require('fs');
-const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require("fs");
+const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds],
+  intents: [GatewayIntentBits.Guilds]
 });
-const { discordToken } = require('./config.json');
-const path = require('path');
+const { discordToken } = require("./config.json");
+const path = require("path");
 
-const commandsDir = './commands';
+const commandsDir = "./commands";
+let commandFiles = [];
 
-client.login(discordToken)
+client
+  .login(discordToken)
   .then(() => {
-    console.log('Bot is now connected to Discord.');
+    console.log("Bot is now connected to Discord.");
   })
-  .catch((error) => {
+  .catch(error => {
     console.error(`Error logging in: ${error}`);
   });
 
-client.once('ready', async () => {
+client.once("ready", async () => {
   console.log(`Logged in as ${client.user.tag}`);
 
   const rest = client.application.commands;
 
-  const commandFiles = fs.readdirSync(commandsDir).filter((file) => file.endsWith('.js'));
+  commandFiles = fs
+    .readdirSync(commandsDir)
+    .filter(file => file.endsWith(".js"));
 
-  const commands = commandFiles.map((file) => {
+  const commands = commandFiles.map(file => {
     const command = require(path.join(__dirname, commandsDir, file));
     return command.data;
   });
 
-  // Register the slash commands
   await rest.set(commands);
 
-  console.log('Successfully registered slash commands.');
+  console.log("Successfully registered slash commands.");
 });
 
-client.on('interactionCreate', async (interaction) => {
+client.on("interactionCreate", async interaction => {
   if (!interaction.isCommand()) return;
 
   const { commandName } = interaction;
-  const commandFile = commandFiles.find((file) => file.startsWith(`${commandName}.js`));
+  const commandFile = commandFiles.find(file =>
+    file.startsWith(`${commandName}.js`)
+  );
 
   if (commandFile) {
     const command = require(path.join(__dirname, commandsDir, commandFile));
     command.execute(interaction);
   } else {
-    interaction.reply('Unknown command. Please use a valid command.');
+    interaction.reply("Unknown command. Please use a valid command.");
   }
 });
