80da5a25821bf11a2e33212ae969c239f7224230
James Timms <59345798+Timmsy1998@users.noreply.github.com>
2023-11-01 21:43:10 +0000

    Updated index.js to have command handling

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
-const config = require('./config.json');
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
-  intents: [
-      GatewayIntentBits.Guilds,
-  ]
-})
+  intents: [GatewayIntentBits.Guilds],
+});
+const { discordToken } = require('./config.json');
+const path = require('path');
 
-const { riotApiKey, discordToken } = config;
+const commandsDir = './commands';
 
 client.login(discordToken)
   .then(() => {
@@ -16,3 +15,35 @@ client.login(discordToken)
   .catch((error) => {
     console.error(`Error logging in: ${error}`);
   });
+
+client.once('ready', async () => {
+  console.log(`Logged in as ${client.user.tag}`);
+
+  const rest = client.application.commands;
+
+  const commandFiles = fs.readdirSync(commandsDir).filter((file) => file.endsWith('.js'));
+
+  const commands = commandFiles.map((file) => {
+    const command = require(path.join(__dirname, commandsDir, file));
+    return command.data;
+  });
+
+  // Register the slash commands
+  await rest.set(commands);
+
+  console.log('Successfully registered slash commands.');
+});
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+  const commandFile = commandFiles.find((file) => file.startsWith(`${commandName}.js`));
+
+  if (commandFile) {
+    const command = require(path.join(__dirname, commandsDir, commandFile));
+    command.execute(interaction);
+  } else {
+    interaction.reply('Unknown command. Please use a valid command.');
+  }
+});
