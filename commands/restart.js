75f548a9671cfed6c148855406a5ec8cda65acbb
James Timms <59345798+Timmsy1998@users.noreply.github.com>
2023-11-01 21:55:59 +0000

    refactored restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -1,18 +1,18 @@
-const { CommandInteraction } = require('discord.js');
+const { CommandInteraction } = require("discord.js");
 
 module.exports = {
   data: {
-    name: 'restart',
-    description: 'Restart the bot.',
+    name: "restart",
+    description: "Restart the bot."
   },
 
   async execute(interaction) {
-    const allowedUserId = '789389975308730398';
+    const allowedUserId = "789389975308730398";
     if (interaction.user.id === allowedUserId) {
-      await interaction.reply('Restarting the bot...');
+      await interaction.reply("Restarting the bot...");
       process.exit(0);
     } else {
-      await interaction.reply('You are not authorized to use this command.');
+      await interaction.reply("You are not authorized to use this command.");
     }
-  },
+  }
 };
